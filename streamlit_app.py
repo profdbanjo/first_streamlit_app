2d6ce537c9b5f2c609305f3a0e2e0b093528adc9
profdbanjo <68724585+profdbanjo@users.noreply.github.com>
2023-01-09 19:59:13 -0800

    Update streamlit_app.py
    updated typo error on my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the table on the page
-streamlit.dataframe(my_fruits_list)
+streamlit.dataframe(my_fruit_list)
